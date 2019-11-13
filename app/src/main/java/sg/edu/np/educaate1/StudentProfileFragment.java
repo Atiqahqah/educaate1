948668bc1e4820c5a3e4a43a845d17ab7412df22
Soon Aik Raymond Neo <s10185503@connect.np.edu.sg>
2019-11-13 17:23:40 +0800

    Merge branch 'master' of https://github.com/Atiqahqah/educaate1
    # Conflicts:
    #	.idea/codeStyles/Project.xml

--- app/src/main/java/sg/edu/np/educaate1/StudentProfileFragment.java
+++ app/src/main/java/sg/edu/np/educaate1/StudentProfileFragment.java
@@ -1,29 +1,110 @@
 package sg.edu.np.educaate1;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StudentProfileFragment extends Fragment {
+    //FireBase
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
+    private FirebaseUser user;
+    private String UID;
 
+    //object and Data
+    private Student student;
+
+    //Views
+    ImageView profilepic;
+    TextView name;
+    ImageButton editprofile;
+    Button summary;
+    Button rating;
+    Button review;
 
     public StudentProfileFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_student_profile, container, false);
+        InitialiseViews(view);
+
+        //get data from firebase
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        UID = user.getUid();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(UID);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                student = dataSnapshot.getValue(Student.class);
+                //Set Data to View
+                name.setText(student.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        editprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),StudentUpdate.class));
+            }
+        });
+
+
+
+        return view;
     }
+
+
+
+
+
+    public void InitialiseViews(View v){
+        name = v.findViewById(R.id.profileNameTV);
+        profilepic = v.findViewById(R.id.profileImage);
+        editprofile = v.findViewById(R.id.updateProfileBtn);
+        summary = v.findViewById(R.id.summaryProfileBtn);
+        rating = v.findViewById(R.id.ratingProfileBtn);
+        review = v.findViewById(R.id.reviewProfileBtn);
+    }
+
+
 
 }
